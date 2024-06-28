eb07622cf92c3f52e571e438e75e87a80323e077
oscarracuna <acunainfosec@proton.me>
2024-06-27 22:15:18 -0600

    added some notations just so I don't forget

--- main.go
+++ main.go
@@ -2,23 +2,45 @@ package main
 
 import (
   "fmt"
-  "net"
-  //"strings"
- // "bufio"
-//  probing "github.com/prometheus-community/pro-bing"
+  "strings"
+  probing "github.com/prometheus-community/pro-bing"
 )
 
 
 func main() {
-  var oct1, oct2, oct3, oct4 byte
-
+  var ip string
   fmt.Print("Enter IP: ")
-  fmt.Scanln(&oct1, &oct2, &oct3, &oct4)
+  fmt.Scanln(&ip)
 
-  ip := net.IPv4(oct1, oct2, oct3, oct4)
+  // This returns an array of the values
+  // [192 168 0 1]
+  // Which we can later call by its index
+  ip_s := strings.Split(ip,".")
 
+
+  // Original ip
   fmt.Println(ip)
-  
-  
+  // Array of the values
+  fmt.Println(ip_s)  
+
+  // When dealing with ranges of arrays[0:2], you get index 0 and 1, but not 2.
+  // For example, in [0:3], you get 0,1 AND 2.
+  oct1 := ip_s[0:2]
+  fmt.Println(oct1)
+
+  pinger, err := probing.NewPinger("google.com")
+  if err != nil {
+    panic(err)
+  }
+
+  // How many packets
+  pinger.Count = 3
+  //You need this to run the pinger
+  pinger.Run()
+
+  // This just prints all of the stats.
+  // You can do pinger.[the specific stat you want]
+  stats := pinger.Statistics()
+  fmt.Println(stats)
 
 }
