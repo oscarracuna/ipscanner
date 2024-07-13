aad6f1b50e88d678388c675879d3ac37d76b1b92
oscarracuna <acunainfosec@proton.me>
2024-07-12 21:15:01 -0600

    beta

--- go.mod
+++ go.mod
@@ -2,9 +2,10 @@ module github.com/oscarracuna/ipscanner
 
 go 1.22.4
 
+require github.com/prometheus-community/pro-bing v0.4.0
+
 require (
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/prometheus-community/pro-bing v0.4.0 // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
--- main.go
+++ main.go
@@ -1,40 +1,40 @@
 package main
 
 import (
-    "fmt"
-    "strconv"
-    "strings"
+	"fmt"
+	"strconv"
+	"strings"
 
-    probing "github.com/prometheus-community/pro-bing"
+	probing "github.com/prometheus-community/pro-bing"
 )
 
 var (
-    ip string
+	ip string
 )
 
 // TODO: Add csv iteration for caps so you can scan ping the entire office with just the cap number
 func main() {
 
-    fmt.Print("Enter IP: ")
-    fmt.Scanln(&ip)
-    ips := strings.Split(ip, ".")
-    threeOctets := ip_s[0:3]
-    preIP := strings.Join(threeOctets, ".")
+	fmt.Print("Enter IP: ")
+	fmt.Scanln(&ip)
+	ip_s := strings.Split(ip, ".")
+	threeOctets := ip_s[0:3]
+	preIP := strings.Join(threeOctets, ".")
 
-    for i := 0; i <= 255; i++ {
-        temp := strconv.Itoa(i)
-        rip := preIP + "." + temp
+	for i := 0; i <= 255; i++ {
+		temp := strconv.Itoa(i)
+		rip := preIP + "." + temp
 
-        pingu,  := probing.NewPinger(rip)
-        pingu.SetPrivileged(true)
-        pingu.Count = 1
-        pingu.Timeout = 1000
-        pingu.Run()
+		pingu, _ := probing.NewPinger(rip)
+		pingu.SetPrivileged(true)
+		pingu.Count = 1
+		pingu.Timeout = 1000
+		pingu.Run()
 
-        stats := pingu.Statistics()
-        rcv := stats.PacketsRecv
-        if rcv >= 1 {
-            fmt.Println("Host alive:", rip)
-        }
-    }
+		stats := pingu.Statistics()
+		rcv := stats.PacketsRecv
+		if rcv >= 1 {
+			fmt.Println("Host alive:", rip)
+		}
+	}
 }
