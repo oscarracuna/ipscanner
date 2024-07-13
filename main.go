0c36f24828a6eb62b7373287958984420b01812d
oscarracuna <acunainfosec@proton.me>
2024-07-12 20:58:50 -0600

    It works now. Minor improvements from now on.

--- main.go
+++ main.go
@@ -1,46 +1,38 @@
-package main
-
 import (
-  "fmt"
-  "strings"
-  probing "github.com/prometheus-community/pro-bing"
+    "fmt"
+    "strconv"
+    "strings"
+
+    probing "github.com/prometheus-community/pro-bing"
 )
 
+var (
+    ip string
+)
 
+// TODO: Add csv iteration for caps so you can scan ping the entire office with just the cap number
 func main() {
-  var ip string
-  fmt.Print("Enter IP: ")
-  fmt.Scanln(&ip)
 
-  // This returns an array of the values
-  // [192 168 0 1]
-  // Which we can later call by its index
-  ip_s := strings.Split(ip,".")
+    fmt.Print("Enter IP: ")
+    fmt.Scanln(&ip)
+    ips := strings.Split(ip, ".")
+    threeOctets := ip_s[0:3]
+    preIP := strings.Join(threeOctets, ".")
 
+    for i := 0; i <= 255; i++ {
+        temp := strconv.Itoa(i)
+        rip := preIP + "." + temp
 
-  // Original ip
-  fmt.Println(ip)
-  // Array of the values
-  fmt.Println(ip_s)  
+        pingu,  := probing.NewPinger(rip)
+        pingu.SetPrivileged(true)
+        pingu.Count = 1
+        pingu.Timeout = 1000
+        pingu.Run()
 
-  // When dealing with ranges of arrays[0:2], you get index 0 and 1, but not 2.
-  // For example, in [0:3], you get 0,1 AND 2.
-  oct1 := ip_s[0:2]
-  fmt.Println(oct1)
-
-  pinger, err := probing.NewPinger("google.com")
-  if err != nil {
-    panic(err)
-  }
-
-  // How many packets
-  pinger.Count = 3
-  //You need this to run the pinger
-  pinger.Run()
-
-  // This just prints all of the stats.
-  // You can do pinger.[the specific stat you want]
-  stats := pinger.Statistics()
-  fmt.Println(stats)
-
+        stats := pingu.Statistics()
+        rcv := stats.PacketsRecv
+        if rcv >= 1 {
+            fmt.Println("Host alive:", rip)
+        }
+    }
 }
