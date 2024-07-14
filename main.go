7c9c2f22bfa9012c24d350207fca3da4476c43df
oscarracuna <acunainfosec@proton.me>
2024-07-14 08:35:47 -0600

    Changed variable names and timeout duration

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 // TODO: Error handleling
 // Infinite loop in case IP is not valid 
 // Add csv iteration for caps so you can scan ping the entire office with just the cap number
+// Possibly add port discovery and output which endpoints may be printers
 
 import (
 	"fmt"
@@ -18,26 +19,27 @@ var (
 
 func main() {
 
-	fmt.Print("Enter IP: ")
+	fmt.Print("Enter LAN IP or VNC: ")
 	fmt.Scanln(&ip)
-	ip_s := strings.Split(ip, ".")
-	threeOctets := ip_s[0:3]
-	preIP := strings.Join(threeOctets, ".")
+	splitIP := strings.Split(ip, ".")
+	arrayOfIP := splitIP[0:3]
+	joinedArrayofIP := strings.Join(arrayOfIP, ".")
 
 	for i := 0; i <= 255; i++ {
+    // This converts ints to ascii
 		temp := strconv.Itoa(i)
-		rip := preIP + "." + temp
+		newIP := joinedArrayofIP + "." + temp
 
-		pingu, _ := probing.NewPinger(rip)
+		pingu, _ := probing.NewPinger(newIP)
 		pingu.SetPrivileged(true)
 		pingu.Count = 1
-		pingu.Timeout = 1000
+		pingu.Timeout = 10*time.Second
 		pingu.Run()
 
 		stats := pingu.Statistics()
 		rcv := stats.PacketsRecv
 		if rcv >= 1 {
-			fmt.Println("Host alive:", rip)
+			fmt.Println("Host alive:", newIP)
 		}
 	}
 }
