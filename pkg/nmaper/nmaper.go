879b793244e6970565d6ac0600acb08a5a2c7056
oscarracuna <acunainfosec@proton.me>
2024-07-21 00:45:11 -0600

    Almost

--- pkg/nmaper/nmaper.go
+++ pkg/nmaper/nmaper.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"time"
   "fmt"
-  "log"
 
 	"github.com/Ullaakut/nmap/v3"
 )
@@ -14,26 +13,17 @@ var (
   porto string
 )
 
-func Nmap(ip string) string{
+func Nmap(ip string) string {
   ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
   defer cancel()
 
-  scanner, err := nmap.NewScanner(
+  scanner, _ := nmap.NewScanner(
     ctx,
     nmap.WithTargets(ip),
     nmap.WithPorts("80,443,8080"),
     )
-  if err != nil {
-    log.Fatalf("Unable to create nmap scanner")
-  }
 
-	result, warnings, err := scanner.Run()
-	if len(*warnings) > 0 {
-		log.Printf("run finished with warnings: %s\n", *warnings)
-	}
-	if err != nil {
-		log.Fatalf("unable to run nmap scan: %v", err)
-	}
+	result, _, _ := scanner.Run()
 
   for _, host := range result.Hosts {
     if len(host.Ports) == 0 || len(host.Addresses) == 0 {
@@ -41,11 +31,9 @@ func Nmap(ip string) string{
     }
   
 	for _, port := range host.Ports {
-      //port := (port.ID, port.Protocol, port.State, port.Service.Name)
-      porto := fmt.Sprint(port.State)
-      fmt.Sprint(porto)
+      fmt.Print(port.State)
+    }
   }
-  }	
   return porto
 }
 
